2080d5f502b3d7e7e4bee520adbfeb23495cf66b
shidayama <shidayama>
2018-02-09 13:58:25 +0900

    Initial Commit

---
+++ illustbook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  illustbook
+//
+//  Created by shidayamasyuuhei on 2018/02/09.
+//  Copyright © 2018年 shidayamasyuuhei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ illustbook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  illustbook
+//
+//  Created by shidayamasyuuhei on 2018/02/09.
+//  Copyright © 2018年 shidayamasyuuhei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
